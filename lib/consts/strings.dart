4da6c5c20780aa56a657f4fb3ef8f0443b4ae877
Muhammad Anas Jamil <167305217+i-anas@users.noreply.github.com>
2024-09-26 16:24:07 +0500

    FashionApp starting work

---
+++ lib/consts/strings.dart
@@ -0,0 +1,3 @@
+const appname = "Fashion Mart";
+const appversion = "Version 1.0.0";
+const credits = "@Anas Jamil";
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Fashion_Mart());
+}
+
+class Fashion_Mart extends StatelessWidget {
+  const Fashion_Mart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
