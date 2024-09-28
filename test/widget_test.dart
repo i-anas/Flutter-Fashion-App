2068ba9f3b8222ae6472bd34ab0ec20413291da6
Muhammad Anas Jamil <167305217+i-anas@users.noreply.github.com>
2024-09-28 12:36:02 +0500

    git ignore files

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:fashion_mart/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const fashionMart());
+    await tester.pumpWidget(const FashionMart());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
