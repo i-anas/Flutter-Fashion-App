2feb8985facf9ded93b3f77f4c341309e785eae2
Muhammad Anas Jamil <167305217+i-anas@users.noreply.github.com>
2024-09-28 13:14:20 +0500

    checking files

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class FashionMart extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(child: Text('Project starting')),
+        body: Center(child: Text('Starting Point')),
       ),
     );
   }
